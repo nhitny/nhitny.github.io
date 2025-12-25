521543a3602322494317a2b8ee159be670f48f2f
nhitny <nhitny2802@gmail.com>
2025-12-25 14:08:34 +0700

    Replace preloader with new spinning loader animation

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,15 @@
 import React from "react";
+
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={props.load ? "preloader" : "preloader-none"}>
+      {props.load && (
+        <div className="loader">
+          <span>Loading...</span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Pre;
