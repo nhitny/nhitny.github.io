50d2f92aed132a6701c3f600d0a50634057cc90f
nhitny <nhitny2802@gmail.com>
2025-12-25 15:43:49 +0700

    Fix animejs import using namespace import

--- src/components/Home/CircuitAnimation.js
+++ src/components/Home/CircuitAnimation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import anime from 'animejs/lib/anime.es.js';
+import * as anime from 'animejs';
 
 const CircuitAnimation = () => {
     const containerRef = useRef(null);
