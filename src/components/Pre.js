1598cc07605d89665f3922c078dcfaa3526a7ac4
nhitny <nhitny2802@gmail.com>
2025-12-25 15:11:48 +0700

    Display both preloaders together: Panda on top, Loading text below

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,41 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function Pre(props) {
-  const [loaderType, setLoaderType] = useState(0);
-
-  useEffect(() => {
-    // Random chọn 1 trong 2 loader khi component mount
-    setLoaderType(Math.floor(Math.random() * 2));
-  }, []);
-
   return (
     <div id={props.load ? "preloader" : "preloader-none"}>
       {props.load && (
-        <>
-          {loaderType === 0 ? (
-            // Panda Loader
-            <div className="panda-container">
-              <div className="ear ear-left" />
-              <div className="ear ear-right" />
-              <div className="panda-head">
-                <div className="eye-patch eye-patch-left" />
-                <div className="eye-patch eye-patch-right" />
-                <div className="eye eye-left" />
-                <div className="eye eye-right" />
-                <div className="pupil pupil-left" />
-                <div className="pupil pupil-right" />
-                <div className="nose" />
-                <div className="mouth" />
-              </div>
+        <div className="preloader-wrapper">
+          {/* Panda Loader - Top */}
+          <div className="panda-container">
+            <div className="ear ear-left" />
+            <div className="ear ear-right" />
+            <div className="panda-head">
+              <div className="eye-patch eye-patch-left" />
+              <div className="eye-patch eye-patch-right" />
+              <div className="eye eye-left" />
+              <div className="eye eye-right" />
+              <div className="pupil pupil-left" />
+              <div className="pupil pupil-right" />
+              <div className="nose" />
+              <div className="mouth" />
             </div>
-          ) : (
-            // Loading Text Loader
-            <div className="text-loader">
-              <span className="loader-text">loading</span>
-              <span className="load" />
-            </div>
-          )}
-        </>
+          </div>
+
+          {/* Loading Text Loader - Bottom */}
+          <div className="text-loader">
+            <span className="loader-text">loading</span>
+            <span className="load" />
+          </div>
+        </div>
       )}
     </div>
   );
