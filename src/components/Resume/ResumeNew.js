4d6d57ad38f39595c86d3a57d9cd6d7ea518adb0
nhitny <nhitny2802@gmail.com>
2025-12-25 15:51:21 +0700

    Replace duplicate Download CV button with Contact Me button

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/CV_Nhitny.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
+import { AiOutlineDownload, AiOutlineMail } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -19,16 +19,24 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row style={{ justifyContent: "center", position: "relative", gap: "15px" }}>
           <Button
             className="download-cv-btn"
-            // variant="primary"
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
+          </Button>
+
+          <Button
+            className="download-cv-btn"
+            href="mailto:nhitny2802@gmail.com"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineMail />
+            &nbsp;Contact Me
           </Button>
         </Row>
 
@@ -36,19 +44,6 @@ function ResumeNew() {
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            className="download-cv-btn"
-            // variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
         </Row>
       </Container>
     </div>
