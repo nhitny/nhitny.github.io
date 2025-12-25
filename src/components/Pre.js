b6143eccfeef31d9ae88628ae0c2e835442590dc
nhitny <nhitny2802@gmail.com>
2025-12-25 15:10:11 +0700

    Add dual preloader: Panda and Loading text animations with white background

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Pre(props) {
+  const [loaderType, setLoaderType] = useState(0);
+
+  useEffect(() => {
+    // Random chọn 1 trong 2 loader khi component mount
+    setLoaderType(Math.floor(Math.random() * 2));
+  }, []);
+
   return (
     <div id={props.load ? "preloader" : "preloader-none"}>
       {props.load && (
-        <div className="loader">
-          <span>Loading...</span>
-        </div>
+        <>
+          {loaderType === 0 ? (
+            // Panda Loader
+            <div className="panda-container">
+              <div className="ear ear-left" />
+              <div className="ear ear-right" />
+              <div className="panda-head">
+                <div className="eye-patch eye-patch-left" />
+                <div className="eye-patch eye-patch-right" />
+                <div className="eye eye-left" />
+                <div className="eye eye-right" />
+                <div className="pupil pupil-left" />
+                <div className="pupil pupil-right" />
+                <div className="nose" />
+                <div className="mouth" />
+              </div>
+            </div>
+          ) : (
+            // Loading Text Loader
+            <div className="text-loader">
+              <span className="loader-text">loading</span>
+              <span className="load" />
+            </div>
+          )}
+        </>
       )}
     </div>
   );
