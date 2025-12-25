dfff17a3f447e98ae69746ab38275100a70d2f67
nhitny <nhitny2802@gmail.com>
2025-12-25 10:25:14 +0700

    Fix: Remove optional react-pdf CSS imports

--- src/components/Education/EducationCards.js
+++ src/components/Education/EducationCards.js
@@ -3,8 +3,6 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
 
 // Setup PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
